48bfe3e0a2dd948ee4bcc3488868a9bccbee5685
developerGatto <83767965+developerGatto@users.noreply.github.com>
2021-05-06 22:41:02 +0200

    Flappy Bird full
    full flappy bird source code in Java

---
+++ flapflap/Renderer.java
@@ -0,0 +1,19 @@
+package flapflap;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+import static flapflap.FlappyBird.flappyBird;
+
+public class Renderer extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+
+    protected void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        FlappyBird.flappyBird.repaint(g);
+    }
+
+}
